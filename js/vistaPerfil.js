dd56ee58474864d856f41df1dce9a782bae8a585
MariacristinaOrtiz <mari_d17@hotmail.com>
2018-01-10 16:31:35 -0500

    Añade vista perfil final

--- js/vistaPerfil.js
+++ js/vistaPerfil.js
@@ -43,10 +43,15 @@ $(document).ready(function() {
     event.preventDefault();
     $('#file-perfil').click();
   });
-  // Evento click para que al hacer click al botton"sube"(perfil) se haga click a input file oculto
+  // Evento click para que al hacer click al botton"subir fotos" se haga click a input file oculto
   $('#file-select-foto').on('click', function(event) {
     event.preventDefault();
     $('#file-foto').click();
+  });
+  // Evento click para que al hacer click al botton"subir fotos para postear" se haga click a input file oculto
+  $('#file-select-post-foto').on('click', function(event) {
+    event.preventDefault();
+    $('#file-post-foto').click();
   });
   // EVENTO PARA AGREGAR PORTADA(foto)
   $('#file-portada').change(function(event) {
@@ -54,7 +59,7 @@ $(document).ready(function() {
     var reader = new FileReader();
     reader.onload = function(event) {
       $('#preview-img').attr('src', event.target.result);
-      $('#fotos-container').append('<img class = "col l4 s3 responsive-img" src= "' + event.target.result + '"/>');
+      $('#fotos-container').append('<img class = "col s4 height= "5vh" responsive-img" src= "' + event.target.result + '"/>');
     };
     reader.readAsDataURL(fileName);
   });
@@ -64,7 +69,7 @@ $(document).ready(function() {
     var reader = new FileReader();
     reader.onload = function(event) {
       $('#perfil-img').attr('src', event.target.result);
-      $('#fotos-container').append('<img class = "col l4 s3 responsive-img" src= "' + event.target.result + '"/>');
+      $('#fotos-container').append('<img class = "col s4 responsive-img"  height= "5vh" src= "' + event.target.result + '"/>');
     };
     reader.readAsDataURL(fileName);
   });
@@ -73,10 +78,21 @@ $(document).ready(function() {
     var fileName = event.target.files[0];
     var reader = new FileReader();
     reader.onload = function(event) {
-      $('#fotos-container').append('<img class = "col l4 s3 responsive-img" src= "' + event.target.result + '"/>');
+      $('#fotos-container').append('<img class = "col s4 height= "5vh" responsive-img" src= "' + event.target.result + '"/>');
     };
     reader.readAsDataURL(fileName);
   });
+  // EVENTO PARA AGREGAR fotos (postear)
+  $('#file-post-foto').change(function(event) {
+    var fileName = event.target.files[0];
+    var reader = new FileReader();
+    reader.onload = function(event) {
+      $containerPost.append('<div class ="col s12 green lighten-5 div-post"><p class ="col s12"> <img class = " col s12 responsive-image-post"src= "' + event.target.result + '"/></p><p class ="col s12">' + new Date().getHours() + ':' + new Date().getMinutes() + ' horas' + '</p></div>');
+      $('#fotos-container').append('<img class = "col s4 responsive-img" height= "5vh" src= "' + event.target.result + '"/>');
+    };
+    reader.readAsDataURL(fileName);
+  });
+
   // .... EVENTOS POSTEAR.....
   // ...Desabilitando el boton publicar...
   $('#textarea1').on('keyup', function() {
@@ -90,10 +106,9 @@ $(document).ready(function() {
   $buttonPublicar.on('click', function(event) {
     event.preventDefault();
     if ($textArea.val().length !== 0) {
-      $containerPost.append('<div class ="col s12 grey lighten-2 div-post"><p class ="col s12"> ' + $textArea.val() + ' </p><p class ="col s12">'+ new Date().getHours() + ':' + new Date().getMinutes() + ' horas' + '</p></div>');
+      $containerPost.append('<div class ="col s12 green lighten-5 div-post"><p class ="col s12"> ' + $textArea.val() + ' </p><p class ="col s12">' + new Date().getHours() + ':' + new Date().getMinutes() + ' horas' + '</p></div>');
     };
     $textArea.val('');
     $buttonPublicar.addClass('disabled', 'disabled');
   });
-
 });
