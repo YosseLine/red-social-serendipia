fc066165ff15a1bba016654565b4f8efb13a6e9c
YosseLine <yoseline_96@hotmail.com>
2018-01-10 18:28:36 -0500

    Corrigiendo error

--- js/index.js
+++ js/index.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
   $('img[alt="logo"]').fadeIn(4000);
   setTimeout(function() {
-    window.location.href = '../views/home.html';
+    window.location.href = '../red-social-serendipia/views/home.html';
   }, 6000);
 });
