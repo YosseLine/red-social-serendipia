434b43571e22d8f8c42c9f773aa77bfd8b80d410
MariacristinaOrtiz <mari_d17@hotmail.com>
2018-01-10 07:50:05 -0500

    Añade funcionalidad para subbir fotos

--- js/vistaPerfil.js
+++ js/vistaPerfil.js
@@ -1,3 +1,37 @@
 $(document).ready(function() {
   $('.dropdown-button').dropdown();
+  // MODAL AMIGOS
+  $('.modal').modal();
+  // SELECCION DE ELEMENTOS GUARDADOS EN VARIBALES
+  var $buttonFotos = $('#fotos');
+  var $thirdSection = $('.third-section');
+  var $secondSection = $('.second-section');
+  var $divContainerButtons = $('#div-container-buttons');
+  console.log($divContainerButtons);
+  // EVENTO click
+  $buttonFotos.on('click', function(event) {
+    event.preventDefault();
+    $secondSection.addClass('hide');
+    $thirdSection.removeClass('hide');
+    $divContainerButtons.append('<a id="biografía"class="waves-effect waves-light btn  green accent-4 ">Biografía</a>');
+    $('#biografía').on('click', function(event) {
+      event.preventDefault();
+      $secondSection.removeClass('hide');
+      $thirdSection.addClass('hide');
+      $('#biografía').remove();
+    });
+  });
+  console.log($('input[type="file"]'));
+  $('input[type="file"]').change(function(e){
+            var fileName = e.target.files[0];
+            var reader = new FileReader();
+            console.log(reader);
+            reader.onload = function(e){
+
+              $('#foto-container').append("<img class = 'col s4 responsive-img circle' src='" + e.target.result + "'/>" );
+
+            }
+            reader.readAsDataURL(fileName);
+        });
+
 });
